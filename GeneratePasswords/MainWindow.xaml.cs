3931218408051ffdcef6058e246ef179dab2c58d
Александр <canych@ukr.net>
2018-03-13 22:33:37 +0300

    Add project files.

---
+++ GeneratePasswords/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GeneratePasswords
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Кол-во генерируемых паролей
+                int count;
+                // Диалог для ввода
+                string c = Microsoft.VisualBasic.Interaction.InputBox("Введите необходимое количество паролей");
+
+                // Проверка корректности ввода
+                if (!int.TryParse(c, out count))
+                {
+                    throw new FormatException();
+                }
+
+                // Список для паролей
+                List<String> passwordList = new List<string>();
+                // строка для пароля
+                string part = "abcdefghijklmnopqrstuvwxyz0123456789";
+                // 
+                Random rnd = new Random();
+
+                for (int i = 0; i < count; i++)
+                {
+					// Временное хранилище для строки
+					StringBuilder tmpStr = new StringBuilder();
+					
+					for (int j = 0; j < 6; j++)
+					{
+						if (j == 2 || j == 4)
+						{
+							tmpStr.Append(part[rnd.Next(26, 36)]);
+						}
+						else
+						{
+							tmpStr.Append(part[rnd.Next(0, 36)]);
+						}
+					}
+					
+					passwordList.Add(tmpStr.ToString());
+					
+                }
+
+                int b = 6;
+				
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Некорректный ввод!", "Ошибка");
+            }
+        }
+    }
+}
