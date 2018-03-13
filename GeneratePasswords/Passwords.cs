c818c465f855dbf41a848cfcc054784e9ed0c5fd
Александр <canych@ukr.net>
2018-03-13 22:52:57 +0300

    Использовал модель

--- GeneratePasswords/Passwords.cs
+++ GeneratePasswords/Passwords.cs
@@ -14,6 +14,15 @@ namespace GeneratePasswords
         // Пароль
         private string _pass;
         public string Pass { get => _pass; set => _pass = value; }
+
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        /// <param name="pass">Пароль</param>
+        public Passwords(string pass)
+        {
+            _pass = pass;
+        }
     }
 
     /// <summary>
