521c08ff7aeb9515262cf130b15fff39e8542bbe
Александр <canych@ukr.net>
2018-03-13 22:38:19 +0300

    Добавлена модель данных

---
+++ GeneratePasswords/Passwords.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratePasswords
+{
+    class Passwords
+    {
+        // Пароль
+        private string _pass;
+        public string Pass { get => _pass; set => _pass = value; }
+    }
+
+    class ListPasswords: List<Passwords>
+    {
+
+    }
+}
